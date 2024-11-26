c6618eb6ef83bca86a6625cd349bf1306e0e9247
Reginaldo Brandão Neto <74476861+netobrandao@users.noreply.github.com>
2024-11-26 06:47:07 -0300

    Update Imigracao_Canadense.py

--- Imigracao_Canadense.py
+++ Imigracao_Canadense.py
@@ -9,7 +9,6 @@ data = pd.read_csv('https://raw.githubusercontent.com/netobrandao/ImmigrationCan
 st.title('Dashboard da Imigração canadense :flag-ca: ')
 st.subheader('Visualize os dados da imigração canadense entre os anos de 1980 a 2013')
 
-
 #tabelas para consulta
 
 #tabela para consulta paises
@@ -28,14 +27,13 @@ devname = pd.DataFrame(data.groupby(by='DevName').sum().sort_values(by='Total',a
 
 aba1,aba2,aba3,aba4,aba5 = st.tabs(["Dados","Paises", "Continentes","Regiões","Categoria"])
 
-#grafico1
-
+#exibir os dados
 with aba1:
     st.header("Imigração Canadense")
     st.write("Visualize dados referentes a imigração canadense.")
     st.dataframe(data,width=2000,height=600,selection_mode='single-row')
 
-
+#grafico1
 with aba2:
     st.header("Gráfico de Barras por Países")
 
@@ -46,16 +44,12 @@ with aba2:
     # Criar gráfico de barras
     if countries:
         fig_country = px.bar(filtered_country, x='Country', y='Total',width=600, height=600)
-        
         fig_country.update_traces(text=filtered_country['Total'],textposition='outside',textfont=dict(size=16))
-        
         fig_country.update_layout(
-    xaxis=dict(
+            xaxis=dict(
         title="Países",
-        titlefont=dict(size=20),  # Tamanho do título do eixo X
-        tickfont=dict(size=18),showgrid=False   # Tamanho dos ticks do eixo X
-    ),yaxis=dict(showgrid=False,title="",tickfont=dict(size=16)))
-        
+        titlefont=dict(size=20), 
+        tickfont=dict(size=18),showgrid=False),yaxis=dict(showgrid=False,title="",tickfont=dict(size=16)))
         st.plotly_chart(fig_country, use_container_width= True)
     else:
         st.warning("Selecione pelo menos um país para visualizar o gráfico.")
@@ -63,15 +57,15 @@ with aba2:
 #grafico2
 with aba3:
     st.header("Gráfico de Barras por Região")
-    # Seleção de países para filtrar
+    # Seleção de regioes para filtrar
     regions = st.multiselect("Selecione as Regiões:", options=region['Region'].unique(), 
                              default=region['Region'][5:8])
     # Filtrar os dados
-    filtered_data = region[region['Region'].isin(regions)]
+    filtered_region = region[region['Region'].isin(regions)]
     #Criar grafico de barras
     if regions:
         fig_region = px.bar(
-                filtered_data,x='Region',y='Total',
+                filtered_region,x='Region',y='Total',
                 width=600, height=600)
         fig_region.update_traces(
                 text=filtered_data['Total'],
@@ -79,12 +73,9 @@ with aba3:
         fig_region.update_layout(
         xaxis=dict(
         title="Regiões",
-        titlefont=dict(size=20),  # Tamanho do título do eixo X
-        tickfont=dict(size=18),showgrid=False   # Tamanho dos ticks do eixo X
-    ),yaxis=dict(showgrid=False,title="",tickfont=dict(size=16)))
-        
-        
-
+        titlefont=dict(size=20), 
+        tickfont=dict(size=18),showgrid=False),
+            yaxis=dict(showgrid=False,title="",tickfont=dict(size=16)))
         st.plotly_chart(fig_region, use_container_width= True)
     else:
         st.warning("Selecione pelo menos uma região para visualizar o gráfico.")
@@ -92,24 +83,23 @@ with aba3:
 #grafico3
 with aba4:
     st.header("Gráfico de Barras por Continente")
-    # Seleção de países para filtrar
+    # Seleção de continentes para filtrar
     continents = st.multiselect("Selecione os Continentes:", options=continent['Continent'].unique(), 
                                 default=continent['Continent'].unique())
     # Filtrar os dados
-    filtered_data = continent[continent['Continent'].isin(continents)]
+    filtered_continent = continent[continent['Continent'].isin(continents)]
     #Criar grafico de barras
     if continents:
-        fig_continent = px.bar(filtered_data,x='Continent',y='Total',
+        fig_continent = px.bar(filtered_continent,x='Continent',y='Total',
                             width=600, height=600)
         fig_continent.update_traces(text=continent['Total'],
                                     textposition='outside',textfont=dict(size=16))
         fig_continent.update_layout(
         xaxis=dict(
         title="Continente",
-        titlefont=dict(size=20),  # Tamanho do título do eixo X
-        tickfont=dict(size=18),showgrid=False   # Tamanho dos ticks do eixo X
-    ),yaxis=dict(showgrid=False,title="",tickfont=dict(size=16)))
-
+        titlefont=dict(size=20),  
+        tickfont=dict(size=18),showgrid=False),
+            yaxis=dict(showgrid=False,title="",tickfont=dict(size=16)))
         st.plotly_chart(fig_continent,use_container_width= True)
     else:
         st.warning("Selecione pelo menos um continente para visualizar o gráfico.")
@@ -121,19 +111,18 @@ with aba5:
     devnames = st.multiselect("Selecione as Categorias:", options=devname['DevName'].unique(), 
                                 default=devname['DevName'].unique())
     # Filtrar os dados
-    filtered_data = devname[devname['DevName'].isin(devnames)]
+    filtered_devname = devname[devname['DevName'].isin(devnames)]
 
     if devnames:
-        fig_devname = px.bar(filtered_data,x='DevName',y='Total',width=600, height=600)
+        fig_devname = px.bar(filtered_devname,x='DevName',y='Total',width=600, height=600)
         fig_devname.update_traces(
         text=continent['Total'],
         textposition='outside',textfont=dict(size=16))
-
         fig_devname.update_layout(
         xaxis=dict(
         title="Categoria",
-        titlefont=dict(size=20),  # Tamanho do título do eixo X
-        tickfont=dict(size=18),showgrid=False   # Tamanho dos ticks do eixo X
+        titlefont=dict(size=20),  
+        tickfont=dict(size=18),showgrid=False   
     ),yaxis=dict(showgrid=False,title="",tickfont=dict(size=16)))
         st.plotly_chart(fig_devname,use_container_width=True)
     else:
